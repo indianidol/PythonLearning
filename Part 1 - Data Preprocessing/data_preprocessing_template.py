5856c054b1a7a38bea41f53ee339e8f381750b52
IndianIdol <kartikraman@gmail.com>
2019-11-13 23:09:59 -0600

    updated Linear regressin

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -16,23 +16,43 @@ y = dataset.iloc[:, 3].values
 
 
 # Fill Missing Values
-from sklearn.impute import SimpleImputer
-missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0,copy="True")
-missingvalues = missingvalues.fit(X.iloc[:, 1:3])
-X.iloc[:, 1:3]=missingvalues.transform(X.iloc[:, 1:3])
+# =============================================================================
+# from sklearn.impute import SimpleImputer
+# missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0,copy="True")
+# missingvalues = missingvalues.fit(X.iloc[:, 1:3])
+# X.iloc[:, 1:3]=missingvalues.transform(X.iloc[:, 1:3])
+# =============================================================================
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-labelencoder_X = LabelEncoder()
-#x[:,0]= labelencoder_X.fit_transform(x[:,0])
-#X.iloc[:,0]= labelencoder_X.fit_transform(X.iloc[:,0])
+#Encoding Categorical Data
+# =============================================================================
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# from sklearn.compose import ColumnTransformer
+# labelencoder_X = LabelEncoder()
+# #x[:,0]= labelencoder_X.fit_transform(x[:,0])
+# #X.iloc[:,0]= labelencoder_X.fit_transform(X.iloc[:,0])
+# 
+# 
+# ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+# #x = np.array(ct.fit_transform(x), dtype=np.float)
+# X = np.array(ct.fit_transform(X), dtype=np.float)
+# # Encoding Y data
+# from sklearn.preprocessing import LabelEncoder
+# y = LabelEncoder().fit_transform(y)
+# =============================================================================
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-#x = np.array(ct.fit_transform(x), dtype=np.float)
-X = np.array(ct.fit_transform(X), dtype=np.float)
-# Encoding Y data
-from sklearn.preprocessing import LabelEncoder
-y = LabelEncoder().fit_transform(y)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+###y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
+
 
 
